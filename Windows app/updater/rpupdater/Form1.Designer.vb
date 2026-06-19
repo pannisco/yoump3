c086042114c407aa46cd2cee01395fd68ffaa8d7
Peppe <gpannisco@gmail.com>
2026-06-19 15:41:21 +0200

    updater 1.1

--- Windows app/updater/rpupdater/Form1.Designer.vb
+++ Windows app/updater/rpupdater/Form1.Designer.vb
@@ -31,45 +31,45 @@ Partial Class Form1
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(19, 122)
-        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ProgressBar1.Location = New System.Drawing.Point(21, 152)
+        Me.ProgressBar1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(461, 28)
+        Me.ProgressBar1.Size = New System.Drawing.Size(519, 35)
         Me.ProgressBar1.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(173, 9)
+        Me.Label1.Location = New System.Drawing.Point(195, 11)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(143, 16)
+        Me.Label1.Size = New System.Drawing.Size(169, 20)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Updating YouMP3..."
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(205, 35)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Location = New System.Drawing.Point(231, 44)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(83, 79)
+        Me.PictureBox1.Size = New System.Drawing.Size(93, 99)
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(493, 163)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(144.0!, 144.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(555, 204)
         Me.ControlBox = False
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ProgressBar1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "Form1"
         Me.Text = "Updating YouMP3..."
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
--- Windows app/updater/rpupdater/Form1.vb
+++ Windows app/updater/rpupdater/Form1.vb
@@ -6,6 +6,7 @@ Imports System.Xml
 Public Class Form1
     Private WithEvents client As WebClient
     Private latestVersion As String = ""
+    Private updVersion As String = "1.1"
     Private zipPath As String = Path.Combine(Application.StartupPath, "temp.zip")
     Private updateurl As String = "https://raw.githubusercontent.com/pannisco/yoump3/refs/heads/main/update.xml"
 
@@ -45,6 +46,13 @@ Public Class Form1
             latestVersion = GetTagValue(data, "version")
             Dim updateLink As String = GetTagValue(data, "link")
             Dim forceUpdate As Boolean = Boolean.Parse(GetTagValue(data, "forceupdate"))
+            Dim updlatest As String = GetTagValue(data, "updtversion")
+            Dim updlink As String = GetTagValue(data, "updtlink")
+            If updlatest > updVersion Then
+                SelfUpdateUpdater(updlink)
+                Return
+            End If
+
             ProgressBar1.Value = 0
             client = New WebClient()
             AddHandler client.DownloadProgressChanged, AddressOf DownloadProgress
@@ -55,6 +63,82 @@ Public Class Form1
             MessageBox.Show("Error during update check: " & ex.Message)
         End Try
     End Sub
+
+    Private Sub SelfUpdateUpdater(updlink As String)
+        Try
+            Dim tempFolder As String = Path.Combine(Path.GetTempPath(), "UpdaterSelfUpdate_" & Guid.NewGuid().ToString("N"))
+            Dim updZipPath As String = Path.Combine(Path.GetTempPath(), "updater_new.zip")
+            Dim extractFolder As String = Path.Combine(tempFolder, "extracted")
+            Dim batchPath As String = Path.Combine(Path.GetTempPath(), "update_updater_" & Guid.NewGuid().ToString("N") & ".bat")
+
+            Dim currentUpdaterFolder As String = Application.StartupPath
+            Dim currentUpdaterExeName As String = Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName)
+            Dim currentUpdaterExePath As String = Path.Combine(currentUpdaterFolder, currentUpdaterExeName)
+            Dim currentPid As Integer = Process.GetCurrentProcess().Id
+
+            Directory.CreateDirectory(tempFolder)
+            Directory.CreateDirectory(extractFolder)
+
+            Using wc As New WebClient()
+                wc.DownloadFile(New Uri(updlink), updZipPath)
+            End Using
+
+            Using zip As ZipFile = ZipFile.Read(updZipPath)
+                For Each entry As ZipEntry In zip
+                    entry.Extract(extractFolder, ExtractExistingFileAction.OverwriteSilently)
+                Next
+            End Using
+
+            CreateUpdateBatchScript(batchPath, currentPid, extractFolder, currentUpdaterFolder, currentUpdaterExePath, tempFolder, updZipPath)
+
+
+            Dim psi As New ProcessStartInfo() With {
+                .FileName = batchPath,
+                .WindowStyle = ProcessWindowStyle.Hidden,
+                .CreateNoWindow = True,
+                .UseShellExecute = True
+            }
+            Process.Start(psi)
+
+            Environment.Exit(0)
+
+        Catch ex As Exception
+            MessageBox.Show("Errore durante l'aggiornamento dell'updater: " & ex.Message)
+        End Try
+    End Sub
+    Private Sub CreateUpdateBatchScript(batchPath As String,
+                                          pidToWaitFor As Integer,
+                                          sourceFolder As String,
+                                          destinationFolder As String,
+                                          exeToRestart As String,
+                                          tempFolderToDelete As String,
+                                          zipToDelete As String)
+
+        Dim sb As New System.Text.StringBuilder()
+
+        sb.AppendLine("@echo off")
+        sb.AppendLine("setlocal")
+        sb.AppendLine()
+        sb.AppendLine(":waitloop")
+        sb.AppendLine($"tasklist /FI ""PID eq {pidToWaitFor}"" 2>NUL | find /I ""{pidToWaitFor}"" >NUL")
+        sb.AppendLine("if not errorlevel 1 (")
+        sb.AppendLine("    timeout /t 1 /nobreak > nul")
+        sb.AppendLine("    goto waitloop")
+        sb.AppendLine(")")
+        sb.AppendLine()
+        sb.AppendLine("timeout /t 1 /nobreak > nul")
+        sb.AppendLine()
+        sb.AppendLine($"xcopy ""{sourceFolder}\*"" ""{destinationFolder}\"" /E /Y /I /Q")
+        sb.AppendLine()
+        sb.AppendLine($"rmdir /S /Q ""{tempFolderToDelete}""")
+        sb.AppendLine($"del /F /Q ""{zipToDelete}""")
+        sb.AppendLine()
+        sb.AppendLine($"start """" ""{exeToRestart}""")
+        sb.AppendLine()
+        sb.AppendLine("(goto) 2>nul & del ""%~f0""")
+        File.WriteAllText(batchPath, sb.ToString(), System.Text.Encoding.Default)
+    End Sub
+
     Private Function GetTagValue(source As String, tag As String) As String
         Dim pattern As String = $"<{tag}>(.*?)</{tag}>"
         Dim match As System.Text.RegularExpressions.Match =
